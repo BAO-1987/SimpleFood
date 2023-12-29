011933f944ac88674e7be3f5c0c4f61f2c247edc
BAO-1987 <andriy.bezkorovajnyj@gmail.com>
2023-12-30 01:21:34 +0200

    dsds

--- src/js/components/_search.js
+++ src/js/components/_search.js
@@ -1,26 +1,26 @@
 
-const modal = document.querySelector(".search");
-const openModalBtn = document.querySelector(".nav__btn");
-const closeModalBtns = document.querySelectorAll('.form__close');
+// const modal = document.querySelector(".search");
+// const openModalBtn = document.querySelector(".nav__btn");
+// const closeModalBtns = document.querySelectorAll('.form__close');
 
-openModalBtn.addEventListener("click", () => {
-  modal.showModal();
-});
+// openModalBtn.addEventListener("click", () => {
+//   modal.showModal();
+// });
 
-closeModalBtns.forEach(closeBtn => {
-  closeBtn.addEventListener("click", () => {
-    modal.close();
-  });
-});
+// closeModalBtns.forEach(closeBtn => {
+//   closeBtn.addEventListener("click", () => {
+//     modal.close();
+//   });
+// });
 
-modal.addEventListener("click", e => {
-  const dialogDimensions = modal.getBoundingClientRect();
-  if (
-    e.clientX < dialogDimensions.left ||
-    e.clientX > dialogDimensions.right ||
-    e.clientY < dialogDimensions.top ||
-    e.clientY > dialogDimensions.bottom
-  ) {
-    modal.close();
-  }
-});
+// modal.addEventListener("click", e => {
+//   const dialogDimensions = modal.getBoundingClientRect();
+//   if (
+//     e.clientX < dialogDimensions.left ||
+//     e.clientX > dialogDimensions.right ||
+//     e.clientY < dialogDimensions.top ||
+//     e.clientY > dialogDimensions.bottom
+//   ) {
+//     modal.close();
+//   }
+// });
--- src/js/functions/_headerScrollHide.js
+++ src/js/functions/_headerScrollHide.js
@@ -1,24 +1,24 @@
-import { _querySelector, _add, _remove } from '../_config.js';
+// import { _querySelector, _add, _remove } from '../_config.js';
 
-let headerHeight = parseFloat(
-  document.documentElement.style.getPropertyValue('--header-height')
-);
+// let headerHeight = parseFloat(
+//   document.documentElement.style.getPropertyValue('--header-height')
+// );
 
-const header = document.querySelector('.header');
-const first = document.querySelector('.main');
-const firstHeight = first.offsetHeight;
-let lastScrollTop = 0;
+// const header = document.querySelector('.header');
+// const first = document.querySelector('.main');
+// const firstHeight = first.offsetHeight;
+// let lastScrollTop = 0;
 
-window.addEventListener('scroll', () => {
-  let scrollDistance = window.scrollY;
+// window.addEventListener('scroll', () => {
+//   let scrollDistance = window.scrollY;
 
-  if (scrollDistance >= 15 + headerHeight) {
-  	header.classList.add('is-scroll');
-  	first.style.paddingTop = headerHeight + 'px';
-  } else {
-  	header.classList.remove('is-scroll');
-  	first.style.paddingTop = null;
-  }
-  lastScrollTop = scrollDistance;
-});
+//   if (scrollDistance >= 15 + headerHeight) {
+//   	header.classList.add('is-scroll');
+//   	first.style.paddingTop = headerHeight + 'px';
+//   } else {
+//   	header.classList.remove('is-scroll');
+//   	first.style.paddingTop = null;
+//   }
+//   lastScrollTop = scrollDistance;
+// });
 
